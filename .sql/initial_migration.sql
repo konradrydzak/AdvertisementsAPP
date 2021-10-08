7e454ed9adeb04fd90ce781ace29bca3ffe48728
Konrad <konradrydzak@gmail.com>
2021-10-08 08:30:04 +0200

    Database from SQLite to PostgreSQL changed

--- .sql/initial_migration.sql
+++ .sql/initial_migration.sql
@@ -1,18 +1,18 @@
-drop table if exists category;
-drop table if exists offer;
+drop table if exists category cascade;
+drop table if exists offer cascade;
 
 CREATE TABLE category (
-	"id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, 
-	"name" varchar(200) NOT NULL, 
-	"ordering" integer unsigned NOT NULL CHECK ("ordering" >= 0)
+	"id" serial NOT NULL PRIMARY KEY,
+	"name" varchar(200) NOT NULL,
+	"ordering" integer NOT NULL CHECK ("ordering" >= 0)
 );
 
 CREATE TABLE offer (
-	"id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, 
-	"title" varchar(200) NOT NULL, 
-	"description" varchar(1000) NOT NULL, 
-	"price" decimal NOT NULL, 
-	"created_at" datetime NOT NULL, 
+	"id" serial NOT NULL PRIMARY KEY,
+	"title" varchar(200) NOT NULL,
+	"description" varchar(1000) NOT NULL,
+	"price" decimal NOT NULL,
+	"created_at" timestamp with time zone NOT NULL,
 	"category_id" int NOT NULL,
 	foreign key ("category_id")
 		references category ("id") on delete cascade
@@ -25,10 +25,10 @@ INSERT INTO category ("name", "ordering") VALUES ('Books', 4);
 INSERT INTO category ("name", "ordering") VALUES ('Job offers', 6);
 INSERT INTO category ("name", "ordering") VALUES ('Films', 5);
 
-INSERT INTO offer ("title", "description", "price", "created_at", "category_id") VALUES ('Apples', 'Apples for sale', 1.50, datetime('now'), 1);
-INSERT INTO offer ("title", "description", "price", "created_at", "category_id") VALUES ('Oranges', 'Oranges for sale', 3.50, datetime('now'), 1);
-INSERT INTO offer ("title", "description", "price", "created_at", "category_id") VALUES ('Fast cars for rent', 'Brand new and exclusive cars for rent, price to be negotiated', 999999, datetime('now'), 3);
-INSERT INTO offer ("title", "description", "price", "created_at", "category_id") VALUES ('Hanabi', 'Great cooperative card game with a twist!', 40, datetime('now'), 2);
-INSERT INTO offer ("title", "description", "price", "created_at", "category_id") VALUES ('Brandon Sanderson Stormlight Archives', 'Amazing fantasy world and a must read!', 250, datetime('now'), 4);
-INSERT INTO offer ("title", "description", "price", "created_at", "category_id") VALUES ('Python Fullstack Developer needed!', 'Tech stack: Django Rest Framework + Angular', 0, datetime('now'), 6);
-INSERT INTO offer ("title", "description", "price", "created_at", "category_id") VALUES ('CODA - great Oscar-worthy film', 'You should definitely watch it', 25, datetime('now'), 5);
+INSERT INTO offer ("title", "description", "price", "created_at", "category_id") VALUES ('Apples', 'Apples for sale', 1.50, NOW(), 1);
+INSERT INTO offer ("title", "description", "price", "created_at", "category_id") VALUES ('Oranges', 'Oranges for sale', 3.50, NOW(), 1);
+INSERT INTO offer ("title", "description", "price", "created_at", "category_id") VALUES ('Fast cars for rent', 'Brand new and exclusive cars for rent, price to be negotiated', 999999, NOW(), 3);
+INSERT INTO offer ("title", "description", "price", "created_at", "category_id") VALUES ('Hanabi', 'Great cooperative card game with a twist!', 40, NOW(), 2);
+INSERT INTO offer ("title", "description", "price", "created_at", "category_id") VALUES ('Brandon Sanderson Stormlight Archives', 'Amazing fantasy world and a must read!', 250, NOW(), 4);
+INSERT INTO offer ("title", "description", "price", "created_at", "category_id") VALUES ('Python Fullstack Developer needed!', 'Tech stack: Django Rest Framework + Angular', 0, NOW(), 6);
+INSERT INTO offer ("title", "description", "price", "created_at", "category_id") VALUES ('CODA - great Oscar-worthy film', 'You should definitely watch it', 25, NOW(), 5);
--- src/advertisements/settings.py
+++ src/advertisements/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+from . import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -74,10 +76,7 @@ WSGI_APPLICATION = 'advertisements.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': config.config()
 }
 
 # Password validation
